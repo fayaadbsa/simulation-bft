15179ee7ffeaa5458f3731131e59f12682c46a5d
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-05-15 18:03:50 +0700

    integration with simulation

---
+++ app/simulator/general.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+class General:
+
+    def __init__(self,
+                 id: int,
+                 is_traitor: bool,
+                 neighbours: list()):
+        
+        self.id = id
+        self.is_traitor = is_traitor
+        self.neighbours = neighbours
+        self.msg_log = dict()
+        self.decision = None
+
+    def send_msg(self, other_general: General, msg: int):
+        
+        if (self.is_traitor):
+            if (msg == 1):
+                other_general.msg_log[f"general_{self.id}"] = 0
+            else:
+                other_general.msg_log[f"general_{self.id}"] = 1
+        
+        else:
+            other_general.msg_log[f"general_{self.id}"] = msg
+
+    
+    def decide_action(self):
+        total_attack = 0
+        total_retreat = 0
+        for k,v in self.msg_log.items():
+            if (v == 1):
+                total_attack += 1
+            else:
+                total_retreat += 1
+
+        if (self.is_traitor):
+            self.decision = 0
+        else:
+            self.decision = 1 if total_attack > total_retreat else 0
+
+
+class SupremeGeneral(General):
+
+    def __init__(self,
+                 id: int,
+                 is_traitor: bool,
+                 neighbours: list(),
+                 order: int):
+        
+        super().__init__(id, is_traitor, neighbours)
+        self.order = order
+        self.decision = None
+
+    
+    def send_msg(self, other_general: General):
+        if (self.is_traitor):
+            if (other_general.id % 2 == 0):
+                other_general.msg_log[f"supreme_general"] = 0
+            else:
+                other_general.msg_log[f"supreme_general"] = 1
+        
+        else:
+            other_general.msg_log[f"supreme_general"] = self.order
+
+    def decide_action(self):
+        if (self.is_traitor):
+            self.decision = 0
+        else:
+            self.decision = self.order
