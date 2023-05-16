4206f834017ad54a84e0309fa4f6870c2dbc2be1
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-05-16 21:41:35 +0700

    update html and view

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.simulator.simulator import main
 
 def index(request):
@@ -6,7 +6,13 @@ def index(request):
     return render(request, "index.html", data)
 
 def result(request):
-    num_general = request.GET.get("general")
-    num_faulty = request.GET.get("faulty")
-    result = main(num_general, num_faulty)
+    num_general = request.GET.get("numGeneral")
+    num_faulty = request.GET.get("numFaulty")
+    is_supreme_traitor = request.GET.get("isSupremeTraitor")
+    order = request.GET.get("order")
+    
+    print(num_general, num_faulty, is_supreme_traitor, order)
+    if (num_general == None or num_faulty == None or is_supreme_traitor == None or order == None) :
+        return redirect("index")
+    result = main(num_general, num_faulty, is_supreme_traitor, order)
     return render(request, "result.html", result)
