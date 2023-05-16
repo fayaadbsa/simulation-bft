e9c6e9632898bca54d1a20f27ea8f0e4d36d181c
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-05-16 22:37:42 +0700

    update landing page and view

--- app/views.py
+++ app/views.py
@@ -3,16 +3,22 @@ from app.simulator.simulator import main
 
 def index(request):
     data = {}
-    return render(request, "index.html", data)
+    return render(request, "landingPage.html", data)
 
 def result(request):
-    num_general = request.GET.get("numGeneral")
-    num_faulty = request.GET.get("numFaulty")
-    is_supreme_traitor = request.GET.get("isSupremeTraitor")
+    num_general = request.GET.get("nodecount")
+    num_faulty = request.GET.get("fnodecount")
+    is_supreme_traitor = request.GET.get("issupremetraitor")
     order = request.GET.get("order")
-    
     print(num_general, num_faulty, is_supreme_traitor, order)
-    if (num_general == None or num_faulty == None or is_supreme_traitor == None or order == None) :
+
+    if (num_general == None or num_faulty == None or order == None) :
         return redirect("index")
+    
+    num_general = int(num_general)
+    num_faulty = int(num_faulty)
+    is_supreme_traitor = True if is_supreme_traitor == "true" else False
+    order = 1 if order == "attack" else 0
+
     result = main(num_general, num_faulty, is_supreme_traitor, order)
     return render(request, "result.html", result)
