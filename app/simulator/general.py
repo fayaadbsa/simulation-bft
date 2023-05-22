c2c3ff34e4e22c726598540d9b86b3b7133968e4
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2023-05-22 17:15:52 +0700

    add log for receiving message

--- app/simulator/general.py
+++ app/simulator/general.py
@@ -8,25 +8,28 @@ class General:
         
         self.id = id
         self.is_traitor = is_traitor
-        self.msg_log = dict()
+        self.send_log = dict()
+        self.receive_log = dict()
         self.decision = None
 
     def send_msg(self, other_general: General, msg: int):
         
         if (self.is_traitor):
             if (msg == 1):
-                other_general.msg_log[f"general_{self.id}"] = 0
+                self.send_log[f"general_{other_general.id}"] = 0
+                other_general.receive_log[f"general_{self.id}"] = 0
             else:
-                other_general.msg_log[f"general_{self.id}"] = 1
+                self.send_log[f"general_{other_general.id}"] = 1
+                other_general.receive_log[f"general_{self.id}"] = 1
         
         else:
-            other_general.msg_log[f"general_{self.id}"] = msg
+            other_general.receive_log[f"general_{self.id}"] = msg
 
     
     def decide_action(self):
         total_attack = 0
         total_retreat = 0
-        for k,v in self.msg_log.items():
+        for k,v in self.receive_log.items():
             if (v == 1):
                 total_attack += 1
             else:
@@ -36,6 +39,8 @@ class General:
             self.decision = 0
         else:
             self.decision = 1 if total_attack > total_retreat else 0
+
+        return {"attack_count": total_attack, "retreat_count": total_retreat, "decision": self.decision}
 
 
 class SupremeGeneral(General):
@@ -47,18 +52,22 @@ class SupremeGeneral(General):
         
         super().__init__(id, is_traitor)
         self.order = order
+        self.send_log = dict()
+        self.receive_log = dict()
         self.decision = None
 
     
     def send_msg(self, other_general: General):
         if (self.is_traitor):
             if (other_general.id % 2 == 0):
-                other_general.msg_log[f"supreme_general"] = 0
+                self.send_log[f"general_{other_general.id}"] = 0
+                other_general.receive_log[f"supreme_general"] = 0
             else:
-                other_general.msg_log[f"supreme_general"] = 1
+                self.send_log[f"general_{other_general.id}"] = 1
+                other_general.receive_log[f"supreme_general"] = 1
         
         else:
-            other_general.msg_log[f"supreme_general"] = self.order
+            other_general.receive_log[f"supreme_general"] = self.order
 
     def decide_action(self):
         if (self.is_traitor):
