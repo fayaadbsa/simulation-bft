ff893f44072878688bd01fac572aedb840f1ee94
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-05-26 05:14:47 +0700

    setup vercel

--- simulation_bft/settings.py
+++ simulation_bft/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'simulation_bft.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
