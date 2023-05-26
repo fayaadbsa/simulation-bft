07b4575dd83f95f221573930f656c96fcdfde9c1
Muhammad Fayaad <muhammad.fayaad@gdn-commerce.com>
2023-05-26 15:33:15 +0700

    refactor

--- app/views.py
+++ app/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render, redirect
 from app.simulator.simulator import main
 
 def index(request):
-    data = {}
-    return render(request, "landingPage.html", data)
+    return render(request, "landingPage.html")
 
 def result(request):
     num_general = request.GET.get("nodecount")
     num_faulty = request.GET.get("fnodecount")
     is_supreme_traitor = request.GET.get("issupremetraitor")
     order = request.GET.get("order")
-    print(num_general, num_faulty, is_supreme_traitor, order)
 
     if (num_general == None or num_faulty == None or order == None) :
         return redirect("index")
